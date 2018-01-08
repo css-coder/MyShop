4c7e28300a79575e2e8489c3a1cf3dbe38b73807
css-coder <moskitiks@gmail.com>
2018-01-08 23:09:54 +0300

    Загрузка проекта

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     *  MyShop - by Skylake
+     * -----------------------------------------------------
+     *  Файл: index.php
+     * -----------------------------------------------------
+     *  Назначение: FRONT CONTROLLER
+     */
+
+    // Вывод ошибок
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    // Подключение файлов системы
+    define('ROOT', dirname(__FILE__));
+    require_once(ROOT . '/components/Router.php');
+
