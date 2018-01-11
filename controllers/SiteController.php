343a988649ccdc3554dcd822c0f4f4bf4f5ce70d
css-coder <moskitiks@gmail.com>
2018-01-11 16:58:18 +0300

    Commit

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,9 +2,11 @@
 
     class SiteController
     {
+
         public function actionIndex()
         {
             require_once (ROOT . '/views/site/index.php');
             return true;
         }
+
     }
