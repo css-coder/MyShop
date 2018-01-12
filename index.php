184fcde996cbc6c9a96b446fe04b2958a0fb7677
css-coder <moskitiks@gmail.com>
2018-01-12 20:09:04 +0300

    Commit

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     // Подключение файлов системы
     define('ROOT', dirname(__FILE__));
     require_once(ROOT . '/components/Router.php');
+    require_once(ROOT . '/components/Db.php');
 
     // Вызов Router
     $router = new Router();
