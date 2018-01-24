000a84a16a9f49f3b21695b4bc77f0e19dcd7371
css-coder <moskitiks@gmail.com>
2018-01-24 15:53:19 +0300

    Commit

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -60,6 +60,7 @@
 
                 $errors = false;
 
+                // Валидация полей
                 if (!User::checkEmail($email)) {
                     $errors[] = 'Неправильный email';
                 }
@@ -68,19 +69,21 @@
                     $errors[] = 'Пароль не должен быть короче 6-ти символов';
                 }
 
+                // Проверяем существует ли пользователь
                 $userId = User::checkUserData($email, $password);
 
                 if ($userId === false) {
+                    // Если данные неправильные - показываем ошибку
                     $errors[] = 'Неправильные данные для входа';
                 }
                 else {
+                    // Если данные правильные, запоминаем пользователя (сессия)
                     User::auth($userId);
 
+                    // Перенаправляем пользователя в закрытую часть - кабинет
                     header("Location: /cabinet/");
 
                 }
-
-
             }
             require_once ROOT . '/views/user/login.php';
 
