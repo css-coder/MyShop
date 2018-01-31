f3c2cd04f0b9ca887d11a87c031128de910a72cb
css-coder <moskitiks@gmail.com>
2018-01-31 21:30:55 +0300

    Добавлена возможность отправки сообщения

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -18,7 +18,30 @@
 
 	    public function actionContacts()
 	    {
-		    echo 'Страница контактов';
+		    $userEmail = '';
+		    $userText = '';
+		    $userSubject = '';
+		    $result = false;
+
+		    if (isset($_POST['submit'])) {
+				$userEmail = $_POST['userEmail'];
+				$userText = $_POST['userText'];
+				$userSubject = $_POST['userSubject'];
+
+				$errors = false;
+
+				if (!User::checkEmail($userEmail)) {
+					$errors[] = 'Не правильный email!!!';
+			    }
+
+			    if ($errors == false) {
+				    $adminEmail = 'l.e.g.i.o.n@inbox.ru';
+				    $subject = $userSubject;
+				    $message = "Содержание письма: {$userText}. от {$userEmail}";
+				    $result = mail($adminEmail, $subject, $message);
+				    $result = true;
+			    }
+		    }
 
 		    require_once ROOT . '/views/site/contacts.php';
 		    return true;
--- views/site/contacts.php
+++ views/site/contacts.php
@@ -1,70 +1,42 @@
 <?php include ROOT . '/views/layouts/header.php'; ?>
 
-	<div id="contact-page" class="container">
-		<div class="bg">
-			<div class="row">
-				<div class="col-sm-12">
-					<h2 class="title text-center">Contact <strong>Us</strong></h2>
-					<div id="gmap" class="contact-map">
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-sm-8">
-					<div class="contact-form">
-						<h2 class="title text-center">Get In Touch</h2>
-						<div class="status alert alert-success" style="display: none"></div>
-						<form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
-							<div class="form-group col-md-6">
-								<input type="text" name="name" class="form-control" required="required" placeholder="Name">
-							</div>
-							<div class="form-group col-md-6">
-								<input type="email" name="email" class="form-control" required="required" placeholder="Email">
-							</div>
-							<div class="form-group col-md-12">
-								<input type="text" name="subject" class="form-control" required="required" placeholder="Subject">
-							</div>
-							<div class="form-group col-md-12">
-								<textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"></textarea>
-							</div>
-							<div class="form-group col-md-12">
-								<input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit">
-							</div>
-						</form>
-					</div>
-				</div>
-				<div class="col-sm-4">
-					<div class="contact-info">
-						<h2 class="title text-center">Contact Info</h2>
-						<address>
-							<p>E-Shopper Inc.</p>
-							<p>935 W. Webster Ave New Streets Chicago, IL 60614, NY</p>
-							<p>Newyork USA</p>
-							<p>Mobile: +2346 17 38 93</p>
-							<p>Fax: 1-714-252-0026</p>
-							<p>Email: info@e-shopper.com</p>
-						</address>
-						<div class="social-networks">
-							<h2 class="title text-center">Social Networking</h2>
-							<ul>
-								<li>
-									<a href="#"><i class="fa fa-facebook"></i></a>
-								</li>
-								<li>
-									<a href="#"><i class="fa fa-twitter"></i></a>
-								</li>
-								<li>
-									<a href="#"><i class="fa fa-google-plus"></i></a>
-								</li>
-								<li>
-									<a href="#"><i class="fa fa-youtube"></i></a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div><!--/#contact-page-->
+    <section>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-sm-4 col-sm-offset-4 padding-right">
+
+					<?php if ($result) { ?>
+                        <p>Сообщение отправлено! Мы ответим Вам на указанный email.</p>
+					<?php } else { ?>
+						<?php if (isset($errors) && is_array($errors)) { ?>
+                            <ul>
+								<?php foreach ($errors as $error) { ?>
+                                    <li> - <?php echo $error; ?></li>
+								<?php } ?>
+                            </ul>
+						<?php } ?>
+
+                        <div class="signup-form"><!--sign up form-->
+                            <h2>Обратная связь</h2>
+                            <h5>Есть вопрос? Напишите нам</h5>
+                            <br/>
+                            <form action="#" method="post">
+                                <input type="email" name="userEmail" placeholder="E-mail" value="<?php echo $userEmail; ?>"/>
+                                <input type="text" name="userSubject" placeholder="Тема письма" value="<?php echo $userSubject; ?>"/>
+                                <textarea name="userText" rows="5" placeholder="Сообщение" value="<?php echo $userText; ?>"/></textarea>
+                                <br/><br/>
+                                <input type="submit" name="submit" class="btn btn-default" value="Отправить" />
+                            </form>
+                        </div><!--/sign up form-->
+					<?php } ?>
+
+
+                    <br/>
+                    <br/>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php include ROOT . '/views/layouts/footer.php'; ?>
